7f08163507c36f498069d1660aaf837225557949
Agustín Herranz <agustin.herranz@gmail.com>
2018-09-07 22:19:03 +0200

    Fix package creation, add console script for app.

---
+++ src/vending/cli.py
@@ -0,0 +1,9 @@
+import click
+from flask.cli import FlaskGroup
+
+from .app import create_app
+
+
+@click.group(cls=FlaskGroup, create_app=create_app)
+def cli():
+    """Management script for the application."""
