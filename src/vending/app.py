5ec382a319b4294e4c1359b08b2618c373fd9381
Agustín Herranz <agustin.herranz@gmail.com>
2018-09-26 21:09:14 +0200

    Add basic HTML pages for emoji vending app.

--- src/vending/app.py
+++ src/vending/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template, redirect, url_for
 import prometheus_client
 
 
@@ -6,8 +6,19 @@ def create_app():
     app = Flask(__name__)
 
     @app.route('/')
-    def hello_world():
-        return 'Hello World!'
+    def landing():
+
+        return render_template("landing.html.j2")
+
+    @app.route('/request-emoji', methods=['POST', 'GET'])
+    def get_emoji():
+
+        return render_template("emoji.html.j2")
+
+    @app.route('/send-feedback', methods=['POST', 'GET'])
+    def send_feedback():
+
+        return redirect(url_for('landing'))
 
     @app.route('/metrics')
     def metrics_endpoint():
